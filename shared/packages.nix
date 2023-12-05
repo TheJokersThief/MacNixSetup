780bfca4db47ace45be2e8af0cbf2aa92e066fc9
Evan <me@iamevan.me>
2023-12-05 18:03:57 +0000

    Remove old zsh theme files

--- shared/packages.nix
+++ shared/packages.nix
@@ -1,52 +1,55 @@
 { pkgs }:
 
 with pkgs; [
-  # General packages for development and system management
-  bash-completion
-  bat
-  btop
-  coreutils
-  killall
+  # # General packages for development and system management
+  # bash-completion
+  # bat
+  # btop
+  # coreutils
+  # killall
+  # mas
+  # just
 
-  # Encryption and security tools
-  age
-  age-plugin-yubikey
-  gnupg
-  libfido2
-  pinentry
-  yubikey-manager
+  # # Encryption and security tools
+  # age
+  # age-plugin-yubikey
+  # gnupg
+  # libfido2
+  # pinentry
+  # yubikey-manager
 
-  # Cloud-related tools and SDKs
-  #
-  # docker marked broken as of Nov 15, 2023
-  # https://github.com/NixOS/nixpkgs/issues/267685
-  #
-  # docker
-  # docker-compose
-  #
-  tflint
+  # # Cloud-related tools and SDKs
+  # #
+  # # docker marked broken as of Nov 15, 2023
+  # # https://github.com/NixOS/nixpkgs/issues/267685
+  # #
+  # # docker
+  # # docker-compose
+  # #
+  # tflint
 
-  # Media-related packages
-  ffmpeg
+  # # Media-related packages
+  # ffmpeg
 
-  # Node.js development tools
-  nodePackages.nodemon
-  nodePackages.prettier
-  nodePackages.npm # globally install npm
-  nodejs
+  # # Node.js development tools
+  # nodePackages.nodemon
+  # nodePackages.prettier
+  # nodePackages.npm # globally install npm
+  # nodejs
 
-  # Text and terminal utilities
-  htop
-  iftop
-  jq
-  ripgrep
-  tree
-  tmux
-  unrar
-  unzip
-  zsh-powerlevel10k
+  # # Text and terminal utilities
+  # htop
+  # iftop
+  # jq
+  # ripgrep
+  # tree
+  # tmux
+  # unrar
+  # unzip
+  # # oh-my-zsh
+  # zsh-powerlevel10k
 
-  # Python packages
-  python39
-  # python39Packages.poetry
+  # # Python packages
+  # python39
+  # # python39Packages.poetry
 ]
